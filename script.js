d6d025b1d3a87062e66a1b3b7ad9e31bcc666fee
wise-guru <wise.guru123@gmail.com>
2022-06-24 23:35:33 -0400

    Add header and sidebar

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+let myLibrary = [];
 
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.info = function() {
+        return (`${title}, ${author} ${pages} ${isRead}`)
+    }
+}
+
+function addBookToLibrary() {
+    
+}
+
+const hobbit = new Book("The Hobbit", "J.R Tolkien", "569", "read")
+
+console.log(hobbit.info());
+
+//Toggle pages read button//
+$('.toggle').click(function(e) {
+    var toggle = this;
+    
+    e.preventDefault();
+    
+    $(toggle).toggleClass('toggle--on')
+           .toggleClass('toggle--off')
+           .addClass('toggle--moving');
+    
+    setTimeout(function() {
+      $(toggle).removeClass('toggle--moving');
+    }, 200)
+  });
