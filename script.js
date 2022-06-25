0dd3f2a109bef5f3272bda4a02ff22fc50a2e16c
wise-guru <wise.guru123@gmail.com>
2022-06-25 14:11:11 -0400

    Complete page layout

--- script.js
+++ script.js
@@ -17,18 +17,3 @@ function addBookToLibrary() {
 const hobbit = new Book("The Hobbit", "J.R Tolkien", "569", "read")
 
 console.log(hobbit.info());
-
-//Toggle pages read button//
-$('.toggle').click(function(e) {
-    var toggle = this;
-    
-    e.preventDefault();
-    
-    $(toggle).toggleClass('toggle--on')
-           .toggleClass('toggle--off')
-           .addClass('toggle--moving');
-    
-    setTimeout(function() {
-      $(toggle).removeClass('toggle--moving');
-    }, 200)
-  });
