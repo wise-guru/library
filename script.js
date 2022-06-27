0f537667911b468c8c194787024bca36569bcafc
wise-guru <wise.guru123@gmail.com>
2022-06-27 18:06:17 -0400

    Create delete functionality

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ const authorText = document.querySelector('#author')
 const pagesText = document.querySelector('#pages')
 const readText = document.querySelector('#read')
 const bookForm = document.querySelector('.book-form')
+const trash = document.querySelector('.trash')
+
+
 
 
 
@@ -19,6 +22,8 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 } 
+
+//Makes sure not to create card unless these conditions are met//
 
 function validateForm(e) {
     e.preventDefault()
@@ -34,15 +39,14 @@ function validateForm(e) {
 function addBookToLibrary(title, author, pages, read) {
     let book = new Book(title, author, pages, read)
     myLibrary.push(book);
-    showBookInfo();
+    showBookInfo(book);
 }
 
 
 
+//To create a new card with every valid submission//
 
-
-function showBookInfo() {
-    for (let i = 0; i < myLibrary.length; i++) {
+function showBookInfo(book) {
         const newCard = document.createElement('div')
         newCard.classList.add('card')
         cards.appendChild(newCard)
@@ -50,31 +54,60 @@ function showBookInfo() {
         const bookTitle = document.createElement('div')
         bookTitle.classList.add('book-title')
         newCard.appendChild(bookTitle)
-        bookTitle.textContent = myLibrary[i].title;
+        bookTitle.textContent = book.title;
 
         const bookAuthor = document.createElement('div')
-        bookAuthor.textContent = myLibrary[i].author;
+        bookAuthor.textContent = book.author;
         bookAuthor.classList.add('book-author')
         newCard.appendChild(bookAuthor)
 
         const bookPages = document.createElement('div')
-        bookPages.textContent = myLibrary[i].pages
+        bookPages.textContent = book.pages
         bookPages.classList.add('book-pages')
         newCard.appendChild(bookPages)
 
         const bookIsRead = document.createElement('div')
-        if(myLibrary[i].read == false) {
+        if(myLibrary.read == false) {
         bookIsRead.textContent = "Not Read";
         } else {
         bookIsRead.textContent = "Read";
             }
         bookIsRead.classList.add('book-read')
         newCard.appendChild(bookIsRead)
-    }
 
-  
+        const cardIcons = document.createElement('div')
+        cardIcons.classList.add('card-icons')
+        newCard.appendChild(cardIcons)
+
+        trashImg = document.createElement('img')
+        trashImg.src = "./img/delete-outline.png"
+        trashImg.classList.add('trash')
+        cardIcons.appendChild(trashImg)
+
+        newCard.setAttribute("data-index", book)
+
+        trashImg.addEventListener('click', function(e) {
+            removeBook(e)
+        } )
+
+        function removeBook(e) {
+            let selectedBook =  e.target.parentNode.parentNode;
+            let selectedIndex = selectedBook.getAttribute("data-index")
+            myLibrary.splice([selectedIndex])
+            cards.removeChild(e.target.parentNode.parentNode)
+            
+        }
 }
+
+
 
 submitBtn.addEventListener('click', function(e) {
     validateForm(e)
 })
+
+
+
+let bookCards = document.querySelectorAll('.card')
+let bookCards_array = Array.from(bookCards)
+
+
